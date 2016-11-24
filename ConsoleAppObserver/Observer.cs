ef234306ef2514d086ed377a04fdec2c509831be
Robert Mitchell <RBalanced@gmail.com>
2016-11-23 19:56:40 -0800

    Added Subject class

--- ConsoleAppObserver/Observer.cs
+++ ConsoleAppObserver/Observer.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppObserver
 {
-    class Observer
+    abstract class Subject
     {
+        private List<Observer> Observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            Observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            Observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer observer in Observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+    abstract class Observer
+    {
+        public abstract void Update();
     }
 }
